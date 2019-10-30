6be3d86dcedd5150b30f62b2ce3b6873d2289bc5
Walda rivers Addam <hisnameisivan@ya.ru>
2019-10-31 01:17:11 +0300

    ft_atoi.c - so slow

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 10:36:25 by waddam            #+#    #+#             */
-/*   Updated: 2019/10/30 00:35:40 by waddam           ###   ########.fr       */
+/*   Updated: 2019/10/31 01:14:51 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@ int		ft_atoi(const char *str)
 {
 	int					sign;
 	unsigned long long	result;
+	unsigned long long	temp;
 
 	sign = 1;
 	result = 0;
@@ -28,11 +29,17 @@ int		ft_atoi(const char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-		result = result * 10 + (*str - '0');
-		if (result > 9223372036854775807 && sign == 1)
+		temp = result * 10;
+		if (((temp / 10) != result) && sign == 1)
 			return (-1);
-		if (result > 9223372036854775808ull && sign == -1)
+		if (((temp / 10) != result) && sign == -1)
 			return (0);
+		result = temp + (*str - '0');
+		// if ((((result * 10) / 10) != result) && sign == 1)
+		// 	return (-1);
+		// if ((((result * 10) / 10) != result) && sign == -1)
+		// 	return (0);
+		// result = result * 10 + (*str - '0');
 		str++;
 	}
 	return ((int)(sign * result));
