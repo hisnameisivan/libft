74adcb9ae5ab43c1fc2011542daca18410bfc8ed
hisnameisivan <hisnameisivan@ya.ru>
2019-11-12 00:28:17 +0300

    libft/ft_atoi_base.c - проходит простые тесты

--- libft/ft_atoi_base.c
+++ libft/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 23:58:07 by waddam            #+#    #+#             */
-/*   Updated: 2019/11/10 01:07:25 by waddam           ###   ########.fr       */
+/*   Updated: 2019/11/12 00:26:52 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,50 @@ static int	ft_pre_atoi(const char **str)
 	return (sign);
 }
 
-/*
-** ft_pre_main_base(char **str, int base) - пропускает префикс и ведущие нули,
-** в зависимости от системы счисления.
-*/
-
-static void	ft_pre_main_base(char **str, int base)
+static void	ft_pre_main_base(const char **str, int base)
 {
 	if (base == 2)
 	{
-		if (ft_strncmp(*str, "0b", 2))
+		if (ft_strncmp(*str, "0b", 2) == 0)
 			(*str) += 2;
-		while (**str == '0')
-			(*str)++;
+		// while (**str == '0')
+		// 	(*str)++;
 	}
-	else if (base == 8)
-	{
-		if (**str == '0')
-			(*str)++;
-	}
+	// else if (base == 8)
+	// {
+	// 	if (**str == '0')
+	// 		(*str)++;
+	// }
 	else if (base == 16)
 	{
-		if (ft_strncmp(*str, "0x", 2) || ft_strncmp(*str, "0X", 2))
+		if (ft_strncmp(*str, "0x", 2) == 0 || ft_strncmp(*str, "0X", 2) == 0)
 			(*str) += 2;
-		while (**str == '0')
-			(*str)++;
+		// while (**str == '0')
+		// 	(*str)++;
 	}
 }
 
 int			ft_atoi_base(const char *str, int str_base)
 {
-	unsigned int	res;
+	unsigned long	result;
 	int				sign;
 
-	if (str_base > 16 || str_base < 2)
+	if (str_base < 2 || str_base > 16 || str == NULL)
 		return (0);
-	res = 0;
+	result = 0;
 	sign = ft_pre_atoi(&str);
 	ft_pre_main_base(&str, str_base);
-	while (*str >= '0' && *str < str_base)
-
-
+	while (*str >= '0' && *str < str_base + '0')
+	{
+		if (*str - '0' <= 10)
+			result = result * str_base + (*str - '0');
+		else
+			result = result * str_base + (*str - ((*str >= 'a') ? 'a' : 'A') + 10);
+		if (result > FT_LONG_MAX && sign == 1)
+			return (-1);
+		if (result > FT_LONG_MAX + 1 && sign == -1)
+			return (0);
+		str++;
+	}
+	return ((int)(sign * result));
 }
