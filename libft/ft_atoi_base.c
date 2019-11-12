dd8d998ba3cd5398604fff2369c425d8b9b2151b
hisnameisivan <hisnameisivan@ya.ru>
2019-11-13 00:34:37 +0300

    libft/ft_atoi_base.c - все тесты пройдены

--- libft/ft_atoi_base.c
+++ libft/ft_atoi_base.c
@@ -6,13 +6,13 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 23:58:07 by waddam            #+#    #+#             */
-/*   Updated: 2019/11/12 01:48:15 by waddam           ###   ########.fr       */
+/*   Updated: 2019/11/13 00:32:57 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_pre_atoi(const char **str, int base)
+static int					ft_pre_atoi(const char **str, int base)
 {
 	int		sign;
 
@@ -34,11 +34,25 @@ static int	ft_pre_atoi(const char **str, int base)
 	return (sign);
 }
 
-int			ft_atoi_base(const char *str, int base)
+static unsigned long long	ft_result_atoi(char num, int base,
+											unsigned long long result)
 {
-	unsigned long	result;
-	int				sign;
+	if (num < 10 + '0' && num < base + '0')
+		result = result * base + (num - '0');
+	else if ((num >= 'a' && num < base + 'a' - 10)
+	|| (num >= 'A' && num <= base + 'A' - 10))
+		result = result * base + (num - ((num >= 'a') ? 'a' : 'A') + 10);
+	return (result);
+}
 
+int							ft_atoi_base(const char *str, int base)
+{
+	int					sign;
+	unsigned long long	result;
+	unsigned long long	limit;
+
+	result = 0;
+	limit = FT_LONG_MAX / 10;
 	if (base < 2 || base > 16 || str == NULL)
 		return (0);
 	result = 0;
@@ -46,14 +60,15 @@ int			ft_atoi_base(const char *str, int base)
 	while ((*str >= '0' && *str <= '9')
 	|| (*str >= 'a' && *str <= 'f') || (*str >= 'A' && *str <= 'F'))
 	{
-		if (*str - '0' <= 10)
-			result = result * base + (*str - '0');
-		else
-			result = result * base + (*str - ((*str >= 'a') ? 'a' : 'A') + 10);
-		if (result > FT_LONG_MAX && sign == 1)
+		if (((*str > base + '0') && base <= 10)
+		|| (ft_islower(*str) && (*str - 'a' > base + '0') && base <= 16)
+		|| (ft_isupper(*str) && (*str - 'A' > base + '0') && base <= 16))
+			break ;
+		if ((result > limit || (result == limit && *str > '7')) && sign == 1)
 			return (-1);
-		if (result > FT_LONG_MAX + 1 && sign == -1)
+		if ((result > limit || (result == limit && *str > '8')) && sign == -1)
 			return (0);
+		result = ft_result_atoi(*str, base, result);
 		str++;
 	}
 	return ((int)(sign * result));
