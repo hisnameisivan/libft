44644729b3ba7079b21ae5405cbfd7060de07812
hisnameisivan <hisnameisivan@ya.ru>
2019-11-22 00:54:00 +0300

    libft/ft_strequ.c - removed unnecessary

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -3,44 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strequ.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waddam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 22:37:52 by waddam            #+#    #+#             */
-/*   Updated: 2018/12/10 22:38:23 by waddam           ###   ########.fr       */
+/*   Updated: 2019/11/22 00:52:10 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_se_base(char const *s1, char const *s2)
-{
-	while (*s1 && *s2)
-	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-			continue ;
-		}
-		return (0);
-	}
-	if ((*s1 > *s2) || (*s1 < *s2))
-		return (0);
-	return (1);
-}
-
 int			ft_strequ(char const *s1, char const *s2)
 {
-	if (s1)
+	if (s1 && s2)
 	{
-		if (s2)
-			return (ft_se_base(s1, s2));
-		else
+		while (*s1 && *s2)
+		{
+			if (*s1 == *s2)
+			{
+				s1++;
+				s2++;
+				continue ;
+			}
 			return (0);
+		}
+		if (*s1 > *s2 || *s1 < *s2)
+			return (0);
+		return (1);
 	}
+	else if (!s1 && !s2)
+		return (1);
 	else
-	{
-		if (s2)
-			return (0);
-		else
-			return (1);
-	}
+		return (0);
 }
