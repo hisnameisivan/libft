9313d5d3c512d62c5d491f965384b447a5ce3a4d
hisnameisivan <hisnameisivan@ya.ru>
2019-11-23 00:16:14 +0300

    libft/ft_strlcat.c - more compact, noDRY

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,40 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: waddam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 22:08:27 by waddam            #+#    #+#             */
-/*   Updated: 2018/12/09 19:19:23 by waddam           ###   ########.fr       */
+/*   Updated: 2019/11/23 00:10:50 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
-	size_t	j;
-	size_t	len_d;
-	size_t	len_s;
+	size_t	dstlen;
+	size_t	srclen;
 
-	i = ft_strlen(dst);
-	j = -1;
-	len_d = i;
-	len_s = ft_strlen(src);
-	if (size == 0)
-		return (len_s);
-	if (size > (len_s + len_d + 1))
-		ft_strcat(dst, src);
-	else
+	dstlen = 0;
+	srclen = 0;
+	while (*(dst + dstlen))
+		dstlen++;
+	while (*(src + srclen))
+		srclen++;
+	if (dstsize > dstlen)
 	{
-		while (i < size - 1)
+		dst += dstlen;
+		dstsize -= dstlen;
+		while (*src && dstsize > 1)
 		{
-			dst[i] = src[++j];
-			i++;
+			dstsize--;
+			*dst++ = *src++;
 		}
-		dst[i] = '\0';
+		*dst = '\0';
 	}
-	if (i >= size)
-		len_d = size;
-	return (len_s + len_d);
+	return ((dstsize > dstlen ? dstlen : dstsize) + srclen);
 }
