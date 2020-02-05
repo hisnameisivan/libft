74310498b3ab2ae279a8f591ac214d09a98f1ff1
hisnameisivan <hisnameisivan@ya.ru>
2020-02-06 01:27:35 +0300

    libft/ft_isupper.c - upper-case character test

--- libft/ft_isupper.c
+++ libft/ft_isupper.c
@@ -6,9 +6,14 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 23:53:58 by waddam            #+#    #+#             */
-/*   Updated: 2019/11/14 23:41:02 by waddam           ###   ########.fr       */
+/*   Updated: 2020/02/06 01:22:56 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** Upper-case character test. Returns zero if the character tests false and
+** returns non-zero if the character tests true
+*/
 
 int		ft_isupper(int c)
 {
