f08dacabff6e96c90eff96f694ab86557b9830b5
hisnameisivan <hisnameisivan@ya.ru>
2020-02-06 01:26:55 +0300

    libft/ft_islower.c - lower-case character test

--- libft/ft_islower.c
+++ libft/ft_islower.c
@@ -6,9 +6,14 @@
 /*   By: waddam <waddam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 23:56:19 by waddam            #+#    #+#             */
-/*   Updated: 2019/11/14 23:50:26 by waddam           ###   ########.fr       */
+/*   Updated: 2020/02/06 01:21:42 by waddam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** Lower-case character test. Returns zero if the character tests false and
+** returns non-zero if the character tests true
+*/
 
 int		ft_islower(int c)
 {
